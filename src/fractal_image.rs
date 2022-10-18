700fa9bc4cd5f4e4b68e7242e14acc24fc2d0d58
MarcusTL12 <marcuspaafjellet@gmail.com>
2022-10-18 13:32:25 +0200

    vec hsv to rgb

--- src/fractal_image.rs
+++ src/fractal_image.rs
@@ -1,4 +1,7 @@
-use std::simd::{LaneCount, Simd, SimdFloat, SupportedLaneCount};
+use std::{
+    f64::consts::PI,
+    simd::{LaneCount, Simd, SimdFloat, SimdPartialEq, SupportedLaneCount},
+};
 
 use image::{Rgb, RgbImage};
 
@@ -18,12 +21,12 @@ where
 }
 
 pub fn test_grad() {
-    let mut img = RgbImage::new(8, 8);
+    let mut img = RgbImage::new(64, 64);
 
-    for i in 0..8 {
-        for j in 0..8 {
-            let r = (i as f64) / 7.0;
-            let b = (j as f64) / 7.0;
+    for i in 0..64 {
+        for j in 0..64 {
+            let r = (i as f64) / 63.0;
+            let b = (j as f64) / 63.0;
             let g = 1.0 - (r + b) / 2.0;
 
             img.put_pixel(
@@ -35,4 +38,68 @@ pub fn test_grad() {
     }
 
     img.save("test.png").unwrap();
+}
+
+pub fn hsv_to_rgb(hsv: [f64; 3]) -> [f64; 3] {
+    let h = 3.0 * hsv[0] / PI;
+    let s = hsv[1];
+    let v = hsv[2];
+
+    let c = v * s;
+    let x = c * (1.0 - (h % 2.0 - 1.0).abs());
+
+    let rgb1 = match (h as usize) % 6 {
+        0 => [c, x, 0.0],
+        1 => [x, c, 0.0],
+        2 => [0.0, c, x],
+        3 => [0.0, x, c],
+        4 => [x, 0.0, c],
+        5 => [c, 0.0, x],
+        _ => unreachable!(),
+    };
+
+    let m = v - c;
+
+    rgb1.map(|x| x + m)
+}
+
+#[inline(always)]
+fn simd_f64_mod2<const LANES: usize>(x: Simd<f64, LANES>) -> Simd<f64, LANES>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    let xi: Simd<u32, LANES> = unsafe { x.to_int_unchecked() };
+    let x2 = xi & Simd::splat(1);
+
+    x2.cast() + (x - xi.cast())
+}
+
+#[inline(always)]
+pub fn hsv_to_rgb_vec<const LANES: usize>(
+    h: Simd<f64, LANES>,
+    s: Simd<f64, LANES>,
+    v: Simd<f64, LANES>,
+) -> [Simd<f64, LANES>; 3]
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    let h = Simd::splat(3.0 / PI) * h;
+
+    let c = v * s;
+    let x =
+        c * (Simd::splat(1.0) - (simd_f64_mod2(h) - Simd::splat(1.0)).abs());
+
+    let hi: Simd<i32, LANES> = unsafe { h.to_int_unchecked() } % Simd::splat(6);
+
+    let hn = [0, 1, 2, 3, 4, 5].map(|x| hi.simd_eq(Simd::splat(x)).cast());
+
+    let m = v - c;
+
+    [((0, 5), (1, 4)), ((1, 2), (0, 3)), ((3, 4), (2, 5))].map(
+        |((i, j), (k, l))| {
+            (hn[i] | hn[j])
+                .select(c, (hn[k] | hn[l]).select(x, Simd::splat(0.0)))
+                + m
+        },
+    )
 }
